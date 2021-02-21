9b31dfae7edb408115d10bcfe60c1145c61e7816
Adam Barron <adambarron@me.com>
2021-02-20 16:20:44 -0800

    set up base of Project component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Project from './components/Project';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Project></Project>
         <About></About>
       </main>
     </div>
---
+++ src/components/Project/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Project() {
+    return (
+        <section>
+            <h1>TITLE</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Project;
